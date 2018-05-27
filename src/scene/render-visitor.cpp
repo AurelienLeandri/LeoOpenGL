34f637e3ec3efc3e5957943bb08001874c5bb03f
AurelienLeandri <aurelien.leandri@gmail.com>
2018-05-27 05:19:56 +0200

    Add camera movements to scene graph with models

--- src/scene/render-visitor.cpp
+++ src/scene/render-visitor.cpp
@@ -13,13 +13,25 @@ RenderVisitor::~RenderVisitor() {
 }
 
 void RenderVisitor::visit(Node *node) {
+  this->_material->use();
+
+  // Transformation matrices
+  glm::mat4 projection = glm::perspective(this->_camera->getZoom(), (float)800/(float)600, 0.1f, 100.0f);
+  glm::mat4 view = this->_camera->getViewMatrix();
+  glUniformMatrix4fv(glGetUniformLocation(this->_material->getProgram(), "projection"), 1, GL_FALSE,
+      glm::value_ptr(projection));
+  glUniformMatrix4fv(glGetUniformLocation(this->_material->getProgram(), "view"), 1, GL_FALSE,
+      glm::value_ptr(view));
+
   this->_visit(node);
 }
 
 void RenderVisitor::_visit(Node *node) {
   GeometryNode * g_node = dynamic_cast<GeometryNode*>(node);
   if (g_node) {
-    this->_material->use();
+    // Draw the loaded model
+    const glm::mat4 &model = g_node->getModelMatrix();
+    glUniformMatrix4fv(glGetUniformLocation(this->_material->getProgram(), "model"), 1, GL_FALSE, glm::value_ptr(model));
     g_node->draw(this->_material);
   }
   for (auto *e : node->getChildren())
