da1c6194c73c5612bddd4cdb8175ed13da28e767
AurelienLeandri <aurelien.leandri@gmail.com>
2018-12-23 20:39:06 +0100

    Add test case for simple cube scene

--- test/scene-graph.cpp
+++ test/scene-graph.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
-#include <renderer/engine.hpp>
-#include <renderer/scene/visitor/transformation-visitor.hpp>
+#include <renderer/renderer.hpp>
+#include <renderer/utils/shader.hpp>
+#include <model/components/material.hpp>
+#include <model/components/drawable-collection.hpp>
+#include <model/components/volume.hpp>
+#include <model/base.hpp>
+
+using namespace leo;
 
 void print_matrix(const glm::mat4x4 &mat) {
   for (int i = 0; i < 4; i++) {
@@ -11,9 +17,29 @@ void print_matrix(const glm::mat4x4 &mat) {
   }
 }
 
+void cubeScene() {
+  // Init scene
+  model::Base b;
+  model::Material material;
+  model::Volume cube = model::Volume::createCube(1.f);
+  model::DrawableCollection drawables;
+  drawables.addDrawable(&cube);
+  material.diffuse_value = glm::vec3(0.89f, 0.42f, 0.11f);
+  b.addComponent("Material", &material);
+  b.addComponent("CubeVolume", &cube);
+  b.addComponent("Drawables", &drawables);
+
+  // Init renderer
+  renderer::Renderer renderer(Shader(
+        "resources/shaders/default.vs.glsl",
+        "resources/shaders/basic.frag.glsl"
+        ));
+
+  // Render
+  renderer.render(&b);
+}
+
 int main()
 {
-  leo::Engine engine;
-  engine.gameLoop();
   return 0;
 }
